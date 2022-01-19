32d3f8d5df0610ad36b3b5803c4946db17f44609
Meenakshi234 <virk.meenakshi@gmail.com>
2022-01-19 18:28:34 -0500

    final code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ /* eslint-env $ */
+
 $(document).ready(function(){
    // create a text node  to handle cross side scripting attack and use this function in any template
   const escape = function(str){
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 
   }
+
   function createTweetElement(tweet)
     {
-        const calDate = timeago.format(tweet.created_at);
-        
-        const newTweet =`
+      const calDate = timeago.format(tweet.created_at);
+      const newTweet =`
         <article class="tweet">
         <header class="tweet--header">
           <image src=${tweet.user.avatars} alt="tweeter-User-Image" class="tweet--avatar"></image>
@@ -27,7 +29,7 @@ $(document).ready(function(){
           <p>${escape(tweet.content.text)}</p>
         </div>
        
-       <footer class="tweet--footer">
+        <footer class="tweet--footer">
         <small class="footer--age">${calDate} <small>
           <span class="footer--actions">
             <a href="#"><i class="fa fa-flag"></i></a>
@@ -41,72 +43,64 @@ $(document).ready(function(){
     return newTweet;
     
     }
-    const renderTweets = function(tweets) {
-            tweets.forEach(function(tweet){
-            const $tweet = createTweetElement(tweet);
-            $("#tweets-container").append($tweet);
-            });
-    
-       }
-    const loadTweets = function(){
-      console.log("load tweets called");
-        $.get("./tweets")      
-        .done(function( data ) {
-          const dataReversed = data.reverse();
-          console.log(dataReversed);
-          renderTweets(dataReversed);
-        }).fail(function(){
-            alert("error");
-        });
-    }
-       loadTweets();
-  
-
-      /*  form submission using jQuery */
-    $("#formTweet").submit(function(event){
-        event.preventDefault();
-        let maxTweetVal = 140;
-        let tweet = $("#form__textarea").val().length;
-        let tweetError = false;
-
-          if(tweet == '' || tweet == null)
-          {
-           // alert("Please enter tweet");
-            tweetError = true;
-
-            $("#newtweet__empty").slideDown("slow");
-            $("#newtweet__maxerror").slideUp("slow");
-           
-          }
-          if( tweet > maxTweetVal)
-          {
-           // alert("Tweet content is too long .")
-            tweetError = true;
-            $("#newtweet__maxerror").slideDown("slow");
-            $("#newtweet__empty").slideUp("slow");
-          }
-          if ( tweetError == true){
-            // alert("Data cannot be submitted due to errors");
-          
-            } else {
-              var formValues = $(this).serialize();
-              $("#newtweet__maxerror").slideUp("slow");
-              $("#newtweet__empty").slideUp("slow");
-              $.ajax(
-              {
-              type:"POST",
-              url:"http://localhost:8080/tweets",
-              data:formValues
-              }).done(function(){
-                $("#form__textarea").val('');
-                loadTweets();
-              })
-             
-          }
-     
+  const renderTweets = function(tweets) {
+    tweets.forEach(function(tweet){
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").append($tweet);
     });
+  }
 
-        
+  const loadTweets = function(){
+    $.get("./tweets",function(data){
+      const dataReversed = data.reverse();
+      renderTweets(dataReversed);
+      });
+  }
+
+// load the tweets 
+  loadTweets();
+  
+/*  form submission using jQuery */
+  $("#formTweet").submit(function(event){
+    let maxTweetVal = 140;
+    let tweet = $("#form__textarea").val().length;
+    let tweetError = false;
+    if(tweet == '' || tweet == null)
+      {
+        tweetError = true;
+        $("#newtweet__empty").slideDown("slow");
+        $("#newtweet__maxerror").slideUp("slow");
+      }
+    if( tweet > maxTweetVal)
+      {
+        tweetError = true;
+        $("#newtweet__maxerror").slideDown("slow");
+        $("#newtweet__empty").slideUp("slow");
+      }
+    if ( !tweetError){
+      var formValues = $(this).serialize();
+      $("#newtweet__maxerror").slideUp("slow");
+      $("#newtweet__empty").slideUp("slow");
+      $.ajax(
+        {
+          type:"POST",
+          url:"http://localhost:8080/tweets",
+          data:formValues
+        })
+        .then(function() {
+          $.get("./tweets").
+          then(function(data){
+            $("#form__textarea").val('');
+            const index = data.length -1;
+            const $tweet=  createTweetElement(data[index]);
+            $("#tweets-container").prepend($tweet);
+          })
+       })
+            
+    }
+    event.preventDefault();
+    });
+     
     
 });
 
